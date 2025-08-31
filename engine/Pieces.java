ae3cf5d0121daefebea2bf26e80bbad6007bd6b5
DarraghOReilly211 <corkishhouse78@gmail.com>
2025-08-31 22:50:37 +0100

    Pushing to keep streak, couldnt work today as I am under the weather

--- engine/Pieces.java
+++ engine/Pieces.java
@@ -53,7 +53,7 @@ public class Pieces {
 
             Next: Set up method for piece movement, and rules per group.
             
-            Note: couldn't do work today as I was under the weather
+            Note: couldn't do work today as I was under the weather. I will continue tomorrow.
         */
 
         String color = "white";
