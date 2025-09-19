12636d9072d3d9738672db4630a75fae06c73477
DarraghOReilly211 <corkishhouse78@gmail.com>
2025-09-19 21:50:25 +0100

    Removing JFrame and JLabel as they will no longer be needed

--- engine/Pieces.java
+++ engine/Pieces.java
@@ -1,7 +1,5 @@
 import java.util.List;
 import java.util.ArrayList;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 
 public class Pieces {
 
@@ -188,24 +186,5 @@ public class Pieces {
         // Example call that uses the injected board without passing it in:
         game.calculateValidMoves();
 
-        JFrame frame = new JFrame("My First JFrame");
-
-        // Create a label
-        JLabel label
-            = new JLabel("Chess JFrame");
-
-        // Add the label to the frame
-        frame.add(label);
-
-        // Set frame properties
-        frame.setSize(300,
-                      200); // Set the size of the frame
-
-        // Close operation
-        frame.setDefaultCloseOperation(
-            JFrame.EXIT_ON_CLOSE);
-
-        // Make the frame visible
-        frame.setVisible(true);
     }
 }
